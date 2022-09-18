d8336318f4fbd7855261c79e4c09105fc1420205
mihaialexandru <alex.mihai.email@gmail.com>
2022-09-18 11:23:33 +0300

    Add operator functionalities

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 const MAX_NUM_DIGITS = 13;
+const MAX_SCIENTIFIC_NOTATION = 6
+
+let currentExpression = {
+    previousOperand: 0,
+    operator: null,
+    currentOperand: 0,
+    existingEquals: false,
+    resetOutput: false
+};
+
+function resetCurrentExpression() {
+    currentExpression.previousOperand = 0;
+    currentExpression.operator = null;
+    currentExpression.currentOperand = 0;
+    currentExpression.existingEquals = false;
+    currentExpression.resetOutput = false;
+}
 
 function updateOutputDigits(text) {
     const output = document.getElementById("result");
-    if (output.textContent === '0') {
+    if (output.textContent === '0' || currentExpression.resetOutput !== false) {
+        currentExpression.resetOutput = false;
         output.textContent = '';
     }
+
     if (output.textContent.length < MAX_NUM_DIGITS) {
         output.textContent += text;
+    }
+    const operand = parseFloat(output.textContent);
+
+    if (currentExpression.operator === null) {
+        currentExpression.previousOperand = operand;
+    }
+    else if (currentExpression.existingEquals === true) {
+        currentExpression.previousOperand = operand;
+        currentExpression.operator = null;
+        currentExpression.currentOperand = 0;
+        currentExpression.existingEquals = false;
+    }
+    else {
+        currentExpression.currentOperand = operand;
     }
 }
 
@@ -41,7 +74,62 @@ function addEqualsEvent() {
 }
 
 function equalsEventHandler() {
+    let result = NaN;
+    if (currentExpression.operator) {
+        let previousOperand = currentExpression.previousOperand;
+        let currentOperand = currentExpression.currentOperand;
+        if (isInScientificNotation(previousOperand) === true) {
+            previousOperand = convertFromScientificNotation(previousOperand);
+        }
+        if (isInScientificNotation(currentOperand) === true) {
+            currentOperand = convertFromScientificNotation(currentOperand);
+        }
+        previousOperand = parseFloat(previousOperand);
+        currentOperand = parseFloat(currentOperand);
+        switch (currentExpression.operator) {
+            case "%":
+                // TODO: implement modulo functionality
+                break;
+            case '/':
+                if (currentOperand === 0) {
+                    // TODO: display error - division by 0!
+                    alert("Division by 0!");
+                }
+                result = previousOperand / currentOperand;
+                break;
+            case '*':
+                result = previousOperand * currentOperand;
+                break;
+            case '-':
+                result = previousOperand - currentOperand;
+                break;
+            case '+':
+                result = previousOperand + currentOperand;
+                break;
+            case null:
+                return;
+            default:
+                break;
+        }
+    }
+    currentExpression.existingEquals = true;
+    if (result.toString().length > MAX_NUM_DIGITS) {
+        if (Math.floor(result) != result) {
+            result = result.toString().substring(0, MAX_NUM_DIGITS);
+        }
+        else {
+            result = result.toExponential(MAX_SCIENTIFIC_NOTATION);
+        }
+    }
+    updateOutputDigits(result);
+}
 
+function isInScientificNotation(number) {
+    return number.toString().includes('e');
+}
+
+function convertFromScientificNotation(number) {
+    return number.toLocaleString("fullwide", { useGrouping: false });
 }
 
 function addModuloEvent() {
@@ -51,7 +139,7 @@ function addModuloEvent() {
 }
 
 function moduloEventHandler() {
-
+    currentExpression.operator = '%';
 }
 
 function addDivisionEvent() {
@@ -61,7 +149,7 @@ function addDivisionEvent() {
 }
 
 function divisionEventHandler() {
-
+    currentExpression.operator = '/';
 }
 
 function addMultiplicationEvent() {
@@ -71,7 +159,7 @@ function addMultiplicationEvent() {
 }
 
 function multiplicationEventHandler() {
-
+    currentExpression.operator = '*';
 }
 
 function addSubtractionEvent() {
@@ -81,7 +169,7 @@ function addSubtractionEvent() {
 }
 
 function subtractionEventHandler() {
-
+    currentExpression.operator = '-';
 }
 
 function addAdditionEvent() {
@@ -92,7 +180,7 @@ function addAdditionEvent() {
 }
 
 function additionEventHandler() {
-
+    currentExpression.operator = '+';
 }
 
 function addPlusMinusEvent() {
@@ -108,6 +196,13 @@ function plusMinusEventHandler() {
     } else if (output.textContent[0] !== '0' || output.textContent.length >= 2) {
         output.textContent = '-' + output.textContent;
     }
+
+    if (currentExpression.currentOperand !== 0) {
+        currentExpression.currentOperand = -currentExpression.currentOperand;
+    }
+    else if (currentExpression.previousOperand !== 0) {
+        currentExpression.previousOperand = -currentExpression.previousOperand;
+    }
 }
 
 function addCommaEvent() {
@@ -118,8 +213,8 @@ function addCommaEvent() {
 
 function commaEventHandler() {
     const output = document.getElementById("result");
-    if (output.textContent.length < 10 && output.textContent.search(',') === -1) {
-        output.textContent += ',';
+    if (output.textContent.length < MAX_NUM_DIGITS && output.textContent.search(/\./) === -1) {
+        output.textContent += '.';
     }
 }
 
@@ -127,6 +222,21 @@ function addBackspaceEvent() {
     document
         .getElementById("backspace")
         .addEventListener("click", backspaceEventHandler);
+}
+
+function removeLastDigitAfterComma(number) {
+    number = parseFloat(number.toString().slice(0, -1));
+    return number;
+}
+
+function removeLastDigit(number) {
+    if (Math.floor(number) !== number) {
+        number = removeLastDigitAfterComma(number);
+    }
+    else {
+        number /= 10;
+    }
+    return number;
 }
 
 function backspaceEventHandler() {
@@ -139,6 +249,13 @@ function backspaceEventHandler() {
     } else if (output.textContent.length === 0) {
         output.textContent = '0';
     }
+
+    if (currentExpression.currentOperand) {
+        currentExpression.currentOperand = removeLastDigit(currentExpression.currentOperand);
+    }
+    else if (currentExpression.previousOperand) {
+        currentExpression.previousOperand = removeLastDigit(currentExpression.previousOperand);
+    }
 }
 
 function addAllClearEvent() {
@@ -150,6 +267,7 @@ function addAllClearEvent() {
 function allClearEventHandler() {
     const output = document.getElementById("result");
     output.textContent = '0';
+    resetCurrentExpression();
 }
 
 function keyEventClick(key) {
@@ -157,6 +275,13 @@ function keyEventClick(key) {
     button.click();
     button.classList.add("active");
     setTimeout(() => button.classList.remove("active"), 90);
+}
+
+function compoundOperatorEquals() {
+    if (currentExpression.operator && currentExpression.previousOperand) {
+        currentExpression.resetOutput = true;
+        document.getElementById("equals").click();
+    }
 }
 
 function keyEventHandler(event) {
@@ -175,21 +300,32 @@ function keyEventHandler(event) {
             keyEventClick(key);
             break;
         case '=':
+            currentExpression.resetOutput = true;
             keyEventClick("equals");
             break;
         case '%':
+            compoundOperatorEquals();
+            currentExpression.resetOutput = true;
             keyEventClick("modulo");
             break;
         case '/':
+            compoundOperatorEquals();
+            currentExpression.resetOutput = true;
             keyEventClick("division");
             break;
         case '*':
+            compoundOperatorEquals();
+            currentExpression.resetOutput = true;
             keyEventClick("multiplication");
             break;
         case '-':
+            compoundOperatorEquals();
+            currentExpression.resetOutput = true;
             keyEventClick("subtraction");
             break;
         case '+':
+            compoundOperatorEquals();
+            currentExpression.resetOutput = true;
             keyEventClick("addition");
             break;
         case 'n':
